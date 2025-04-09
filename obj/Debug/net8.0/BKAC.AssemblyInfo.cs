ae3c65446f0bd9aca5a3522c2d640e0fbf8fe392
nguyends-bka <nguyends.bka@gmail.com>
2025-04-09 18:21:52 +0700

    update port 8090

--- obj/Debug/net8.0/BKAC.AssemblyInfo.cs
+++ obj/Debug/net8.0/BKAC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BKAC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17d02ae9ba8ef106fd3e20136244c50b5f9e5a01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff5686ecf29d8bf9b15585e24c9c9b61d85f85b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BKAC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BKAC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
