c815a99cff41dba8d8342d99a9014e84ad9219a3
ngocmx <ngocmx@soict.hust.edu.vn>
2025-04-11 09:48:01 +0700

    ok

--- Models/User.cs
+++ Models/User.cs
@@ -10,9 +10,9 @@ namespace BKAC.Models
         public string Fingerprint { get; set; } = string.Empty;  // Ảnh vân tay (Có thể lưu dưới dạng đường dẫn file hoặc base64)
 
         // Khởi tạo Id mặc định là Guid mới khi khởi tạo đối tượng
-        // public User()
-        // {
-        //     Id = Guid.NewGuid().ToString();
-        // }
+        public User()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
     }
 }
