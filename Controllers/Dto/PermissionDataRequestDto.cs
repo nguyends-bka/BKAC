8090a4800f60c538f005d710f12e91788e15e2a6
ngocmx <ngocmx@soict.hust.edu.vn>
2025-04-13 09:51:51 +0700

    batch create permission

---
+++ Controllers/Dto/PermissionDataRequestDto.cs
@@ -0,0 +1,10 @@
+namespace BKAC.Controllers.Dto
+{
+    public class PermissionDataRequestDto
+    {
+        public List<string> UserIds { get; set; }
+        public List<string> DeviceIds { get; set; }
+        public DateTime Time_Start { get; set; }
+        public DateTime Time_End { get; set; }
+    }
+}
